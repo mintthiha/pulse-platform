4c6c8782104424fa99d23529a5e09e36ddbcd18c
mintthiha <mintthiha@gmail.com>
2026-03-07 23:31:16 -0500

    Allow request for vercel url

--- src/main/java/com/pulse/common/config/CorsConfig.java
+++ src/main/java/com/pulse/common/config/CorsConfig.java
@@ -18,6 +18,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://pulse-frontend-psi.vercel.app");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
